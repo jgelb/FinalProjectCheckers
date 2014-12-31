45855d58705aa43e0741f3fb43a6dd573366bd3e
jgelb <jgelb235@verizon.net>
2014-12-30 21:56:25 -0500

    Worked on GetMove function

--- board.java
+++ board.java
@@ -1,6 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
 public class board{
     private char[][] board = new char[9][18];
-    
+    public int moveRow = 0;
     public board(){
 	int k = 1;
 	for (int i=0;i<board.length;i++){
@@ -108,9 +112,47 @@ public class board{
 	}
 	return s;
     }
-
+    //Here will be the code that takes the user move input, and converts it to a spot in the array (for example, H8 is board[0][15])
+    public int getMoveRow(){
+        
+	System.out.println("");
+        System.out.println ("First enter a valid letter of the row to which you want to move");
+	Scanner scanner= new Scanner(System.in);
+	String Action = scanner.nextLine();
+	if (Action.equals("H")){
+	    moveRow = 0;
+	}
+	else if (Action.equals("G")){
+	    moveRow = 1;
+	}
+	else if (Action.equals("F")){
+	    moveRow = 2;
+	}
+	else if (Action.equals("E")){
+	    moveRow = 3;
+	}
+	else if (Action.equals("D")){
+	    moveRow = 4;
+	}
+	else if (Action.equals("C")){
+	    moveRow = 5;
+	}
+	else if (Action.equals("B")){
+	    moveRow = 6;
+	}
+	else if (Action.equals("A")){
+	    moveRow = 7;
+	}
+	else getMoveRow();
+	return moveRow;
+    }
+	    
+	
+	
+        
     public static void main(String[] args){
 	board x = new board();
 	System.out.println(x);
+	x.getMoveRow();
     }
 }
